39b706d6e7b70e8f4eacaffb9c5114dc9d93d834
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 17:29:07 +0100

    using `DOM` API with canvas, SVG or image file

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-if (confirm("Are you sure you want to delete this?")) {
-  console.log("Item deleted");
-}
+var canvas = document.createElement("canvas");
+canvas.width = 500;
+canvas.height = 250;
+
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Cursive";
+ctx.fillText("Hello, World!", 50, 50);
+
+document.body.appendChild(canvas);
+
+var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+svg.width = 500;
+svg.height = 50;
+
+var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+text.setAttribute("x", "0");
+text.setAttribute("y", "50");
+text.style.fontFamily = "Times New Roman";
+text.style.fontSize = "50";
+text.textContent = "Hello, World!";
+svg.appendChild(text);
+document.body.appendChild(svg);
+
+var img = new Image();
+img.src = "https://i.ytimg.com/vi/zecueq-mo4M/maxresdefault.jpg";
+document.body.appendChild(img);
