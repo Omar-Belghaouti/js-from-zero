dc61d92bcf9bd7a0d11cd8fc1691db6b88e56aff
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 17:17:39 +0100

    using `window.prompt()`

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-window.alert("Hello, World!");
-
-// since window is a global object we can directly use its properties like alert
-
-alert("Hi");
-console.log("Alert was dismissed");
+var age = prompt("How old are you?");
+console.log(age);
