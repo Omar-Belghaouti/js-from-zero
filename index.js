6f7567ce04d13e286bed1514058fe71e864052c3
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 19:03:39 +0100

    counting with `console.count()`

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
-console.table(["Hello", "World"]);
+var o1 = 1,
+  o2 = "2",
+  o3 = "";
+console.count(o1);
+console.count(o2);
+console.count(o3);
+console.count(1);
+console.count("2");
+console.count("");
 
-console.table({ foo: "bar", bar: "baz" });
+console.count(42.3);
+console.count(Number("42.3"));
+console.count("42.3");
 
-var personArr = [
-  {
-    personId: 123,
-    name: "Jhon",
-    city: "Melbourne",
-    phoneNo: "1234567890",
-  },
-  {
-    personId: 124,
-    name: "Amelia",
-    city: "Sydney",
-    phoneNo: "1234567890",
-  },
-  {
-    personId: 125,
-    name: "Emily",
-    city: "Perth",
-    phoneNo: "1234567890",
-  },
-  {
-    personId: 126,
-    name: "Abraham",
-    city: "Perth",
-    phoneNo: "1234567890",
-  },
-];
+console.count(console.constructor);
+console.count(function () {});
+console.count(Object);
+var fn1 = function myfn() {};
+console.count(fn1);
+console.count(Number);
 
-console.table(personArr, ["name", "personId"]);
+console.count(undefined);
+console.count(document.Batman);
+var obj;
+console.count(obj);
+console.count(Number(undefined));
+console.count(NaN);
+console.count(NaN + 3);
+console.count(1 / 0);
+console.count(String(1 / 0));
+console.count(window);
+console.count(document);
+console.count(console);
+console.count(console.__proto__);
+console.count(console.constructor.prototype);
+console.count(console.__proto__.constructor.prototype);
+console.count(Object.getPrototypeOf(console));
+console.count(null);
