370c82e2fe67d0e03293ffe0d465451a35b17c9a
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 18:13:23 +0100

    `Number` constants

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-console.log(1 / 0);
-console.log(-Infinity);
-
-console.log(Infinity > 123192310293);
-console.log(-Infinity < -123192310293);
-console.log(Math.pow(123123123, 9123192391023));
-console.log(Number.MAX_VALUE * 2);
-console.log(23 / Infinity);
-console.log(-Infinity === Number.NEGATIVE_INFINITY);
-console.log(-0);
-console.log(0 === -0);
-console.log(1 / -0);
-console.log(1 / 0 === 1 / -0);
-console.log(Infinity + Infinity);
+console.log(Number.MAX_VALUE);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MIN_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.EPSILON);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.NaN);
