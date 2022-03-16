4b540d5e7f8e8403868a19accca3babf89ce9d6d
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 18:20:25 +0100

    using comments

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-console.log(Math.floor("a"));
-console.log(Math.sqrt(-1));
+function elementAt(event) {
+  // Gets the element from Even coordinates
+  return document.elementFromPoint(event.clientX, event.clientY);
+}
+// TODO: write more cool stuff!
+
+/*
+    Gets the element from Event coordinates.
+    Use like:
+    var clickedEl = someEl.addEventListener("click", elementAt2, false);
+*/
+function elementAt2(event) {
+  return document.elementFromPoint(event.clientX, event.clientY);
+}
+
+/* TODO: write more useful comments */
