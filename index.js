cac21f440163afd05bc5516d39e55013badf5afb
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 17:33:27 +0100

    using a variable

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var myVariable = "This is a variable!";
+var number1 = 5;
+number1 = 3;
+console.log(number1);
+
+number1 = number1 + 5;
+number1 = number1 - 6;
+var number2 = number1 * 10;
+var number3 = number2 / number1;
+
+var myString = "I am a " + "string!";
