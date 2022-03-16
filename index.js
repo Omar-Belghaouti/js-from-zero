8c390c8e09fac68dafc2c65c16d7c0a82f334f95
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 17:09:36 +0100

    using the `DOM` API

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-var foo = "bar";
-console.log(foo);
+setTimeout(() => {
+  var paragraph = document.getElementById("paragraph");
+  paragraph.textContent = "Hello, World";
 
-var thisVar = "first value";
-var thatVar = "second value";
-console.log("thisVar:", thisVar, "and thatVar:", thatVar);
+  var element = document.createElement("h1");
+  element.textContent = "Element added from JS";
+  document.body.prepend(element);
 
-var greet = "Hello",
-  who = "World";
-console.log("%s, %s!", greet, who);
-
-console.log({
-  Email: "",
-  Groups: {},
-  Id: 33,
-  IsHiddenInUI: false,
-  IsSiteAdmin: false,
-  LoginName: "i:0#.w|virtualdomain\\user2",
-  PrincipalType: 1,
-  Title: "user2",
-});
-
-console.log(module);
+  var secondElement = document.createElement("h2");
+  secondElement.textContent = "Added at the end";
+  document.body.appendChild(secondElement);
+}, 0);
