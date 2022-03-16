c86cee79ec6ef4d5f04165ab70988ec5d42304e7
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 20:26:56 +0100

    trim whitespace

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-function wordCount(val) {
-  var wom = val.match(/\S+/g);
-  return {
-    charactersNoSpaces: val.replace(/\s+/g, "").length,
-    characters: val.length,
-    words: wom ? wom.length : 0,
-    lines: val.split(/\r*\n/).length,
-  };
-}
+var str = "      some whitespaced string      ";
+console.log(str.trim());
 
-var someMultilineText = "Hello World\n How are you?";
-console.log(wordCount(someMultilineText));
+console.log(str.trimEnd());
