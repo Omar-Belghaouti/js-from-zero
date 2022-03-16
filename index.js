a119a37896a2eec54e1cd0085082a34fe61401ef
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 18:10:49 +0100

    `Infinity` and `-Infinity`

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-console.log(typeof undefined);
-console.log(typeof null);
+console.log(1 / 0);
+console.log(-Infinity);
 
-let foo;
-console.log("is undefined?", foo === undefined);
-
-foo = { a: "a" };
-console.log("is undefined?", foo.b === undefined);
-
-function bar() {
-  return;
-}
-console.log("is undefined?", bar() === undefined);
-
-function foobar(param) {
-  console.log("is undefined?", param === undefined);
-}
-foobar("a");
-foobar();
-
-console.log(window.hasOwnProperty(undefined));
+console.log(Infinity > 123192310293);
+console.log(-Infinity < -123192310293);
+console.log(Math.pow(123123123, 9123192391023));
+console.log(Number.MAX_VALUE * 2);
+console.log(23 / Infinity);
+console.log(-Infinity === Number.NEGATIVE_INFINITY);
+console.log(-0);
+console.log(0 === -0);
+console.log(1 / -0);
+console.log(1 / 0 === 1 / -0);
+console.log(Infinity + Infinity);
