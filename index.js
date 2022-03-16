f1d835ee3759db1aeaf5ed7e3523c7894c1a6711
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 20:16:35 +0100

    access character at index in string

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-var a = "hello";
-var b = "world";
-console.log(a.localeCompare(b)); // -1
+var string = "Hello, World!";
+console.log(string.charAt(4));
+console.log(string[4]);
 
-function strcmp(a, b) {
-  if (a === b) {
-    return 0;
-  }
-  if (a > b) {
-    return 1;
-  }
-  return -1;
-}
-console.log(strcmp("hello", "world")); // -1
-console.log(strcmp("hello", "hello")); // 0
-console.log(strcmp("world", "hello")); // 1
-
-var arr = ["bananas", "cranberries", "apples"];
-arr.sort(function (a, b) {
-  return a.localeCompare(b);
-});
-console.log(arr);
+console.log(string.charCodeAt(4));
