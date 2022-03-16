855b90bc753d590889291c3f8989a3f522d431e4
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 17:44:03 +0100

    `null`

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-var myArray = [];
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(typeof null);
 
-var favoriteFruits = ["apple", "orange", "strawberry"];
-var carsInParkingLot = ["Toyota", "Ferrari", "Lexus"];
-var employees = ["Billy", "Bob", "Joe"];
-var primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
-var randomVariables = [2, "any type works", undefined, null, true, 2.51];
-
-myArray = ["zero", "one", "two"];
-console.log(myArray[0]);
-
-myArray = ["John Doe", "Billy"];
-elementNumber = 1;
-console.log(myArray[elementNumber]);
-
-myObject = {};
-john = {
-  firstname: "John",
-  lastname: "Doe",
-  fullname: "John Doe",
-};
-console.log(john.fullname);
+var a = null;
+console.log(a === null);
