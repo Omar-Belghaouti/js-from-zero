d9c4c11d58850e63e6291b00ea0a94d028288fa4
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 18:16:50 +0100

    `Math` library functions that return `NaN`

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-console.log("b" * 3);
-console.log("cde" - "e");
-console.log([1, 2, 3] * 2);
-console.log([2] * [3]);
-console.log("a" + "b");
-console.log(0 / 0);
+console.log(Math.floor("a"));
+console.log(Math.sqrt(-1));
