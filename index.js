c8d805fc439234813958425d14e027091f57799b
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 19:14:33 +0100

    `typeof`

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.assert("one" === 1, "Testing assertion...");
+console.log(typeof "String");
+console.log(typeof Date(1998, 03, 19));
+
+console.log(typeof 42);
+
+console.log(typeof true);
+
+console.log(typeof {});
+console.log(typeof []);
+console.log(typeof null);
+console.log(typeof /aaa/);
+console.log(typeof Error());
+
+console.log(typeof function () {});
+
+var var1;
+console.log(typeof var1);
