a419b54cc85ec52cc98b1fa87bdfe0d895545979
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 20:10:55 +0100

    reverse string

--- index.js
+++ index.js
@@ -1,33 +1,19 @@
-var hello = "Hello";
-var world = "world";
-var helloW = `${hello} ${world}`;
-console.log(helloW);
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
 
-var intString = String(32);
-var booleanString = String(true);
-var nullString = String(null);
-console.log(intString, booleanString, nullString);
+function reverseString2(str) {
+  return [...str].reverse().join("");
+}
 
-var intString2 = (5232).toString();
-var booleanString2 = false.toString();
-var objString = {}.toString();
-console.log(intString2, booleanString2, objString);
+function reverseString3(str) {
+  let strRev = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    strRev += str[i];
+  }
+  return strRev;
+}
 
-console.log(String.fromCharCode(104, 101, 108, 108, 111));
-
-var objectString = new String("Yes, I am a String object");
-console.log(typeof objectString);
-console.log(typeof objectString.valueOf());
-
-var foo = "Foo";
-var bar = "Bar";
-console.log(foo + bar); // => "FooBar" console.log(foo + " " + bar); // => "Foo Bar"
-foo.concat(bar); // => "FooBar" "a".concat("b", " ", "d") // => "ab d"
-
-var string = "string";
-var number = 32;
-var boolean = true;
-console.log(string + number + boolean); // "string32true"
-
-console.log(`a\\b`);
-console.log(String.raw`a\\b`);
+console.log(reverseString("string"));
+console.log(reverseString2("string"));
+console.log(reverseString3("string"));
