4535f1b8cb5a9733b005aea5eb62b548e802491e
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 17:15:00 +0100

    using `window.alert()`

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-setTimeout(() => {
-  var paragraph = document.getElementById("paragraph");
-  paragraph.textContent = "Hello, World";
+window.alert("Hello, World!");
 
-  var element = document.createElement("h1");
-  element.textContent = "Element added from JS";
-  document.body.prepend(element);
+// since window is a global object we can directly use its properties like alert
 
-  var secondElement = document.createElement("h2");
-  secondElement.textContent = "Added at the end";
-  document.body.appendChild(secondElement);
-}, 0);
+alert("Hi");
+console.log("Alert was dismissed");
