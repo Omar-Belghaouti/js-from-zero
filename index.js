d4b4c35bb90b75339deedbc178d40fba9269da21
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 20:35:57 +0100

    detecting a string

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-var s = "some ∆≈ƒ unicode ¡TM£¢¢¢";
-console.log(s.charCodeAt(5)); // 8710
+var aString = "my string";
+var anInt = 5;
+var anObj = {};
+
+console.log(typeof aString === "string");
+console.log(typeof anInt === "string");
+console.log(typeof anObj === "string");
+
+var aStringObj = new String("my string");
+console.log(typeof aStringObj);
+console.log(aStringObj instanceof String);
+
+var isString = function (value) {
+  return typeof value === "string" || value instanceof String;
+};
+
+var aString = "Primitive String";
+var aStringObj = new String("String Object");
+console.log(isString(aString)); // true
+console.log(isString(aStringObj)); // true
+console.log(isString({})); // false
+console.log(isString(5)); // false
+
+var pString = "Primitive String";
+var oString = new String("Object Form of String");
+console.log(Object.prototype.toString.call(pString)); //"[object String]"
+console.log(Object.prototype.toString.call(oString)); //"[object String]"
+
+var aString = "Primitive String";
+// Generic check for a substring method
+if (aString.substring) {
+}
+// Explicit check for the String substring prototype method
+if (aString.substring === String.prototype.substring) {
+  aString.substring(0);
+}
