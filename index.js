8a520143b34dddfd2a9280b62335890e8d532d88
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-16 18:05:09 +0100

    `undefined` and `null`

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-console.log(window.hasOwnProperty("NaN"));
-console.log(typeof NaN);
-console.log(NaN == NaN);
-console.log(NaN === NaN);
+console.log(typeof undefined);
+console.log(typeof null);
+
+let foo;
+console.log("is undefined?", foo === undefined);
+
+foo = { a: "a" };
+console.log("is undefined?", foo.b === undefined);
+
+function bar() {
+  return;
+}
+console.log("is undefined?", bar() === undefined);
+
+function foobar(param) {
+  console.log("is undefined?", param === undefined);
+}
+foobar("a");
+foobar();
+
+console.log(window.hasOwnProperty(undefined));
