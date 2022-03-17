f59888eb0150c9034d9ab96bc0d90fe023881c33
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 14:26:50 +0100

    creating a `Date` from `UTC`

--- index.js
+++ index.js
@@ -1,25 +1,88 @@
-var date1 = new Date();
+// Naive approach
+function formatDate(dayOfWeek, day, month, year) {
+  var daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  return daysOfWeek[dayOfWeek] + " " + months[month] + " " + day + " " + year;
+}
+//Foo lives in a country with timezone GMT + 1
+var birthday = new Date(2000, 0, 1);
+console.log(
+  "Foo was born on: " +
+    formatDate(
+      birthday.getDay(),
+      birthday.getDate(),
+      birthday.getMonth(),
+      birthday.getFullYear()
+    )
+);
 
-console.log(date1.toString());
+// Correct approach
+function formatDate2(dayOfWeek, day, month, year) {
+  var daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  var months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  return daysOfWeek[dayOfWeek] + " " + months[month] + " " + day + " " + year;
+}
+//Foo lives in a country with timezone GMT + 1
+var birthday = new Date(Date.UTC(2000, 0, 1));
+console.log(
+  "Foo was born on: " +
+    formatDate2(
+      birthday.getUTCDay(),
+      birthday.getUTCDate(),
+      birthday.getUTCMonth(),
+      birthday.getUTCFullYear()
+    )
+);
 
-console.log(date1.toTimeString());
+console.log(Date.UTC(2000, 0, 31, 12));
 
-console.log(date1.toDateString());
+var utcDate = new Date(Date.UTC(2000, 0, 31, 12));
+console.log(utcDate);
 
-console.log(date1.toUTCString());
+var utcDate = new Date(Date.UTC(2000, 0, 31, 12));
+var localDate = new Date(2000, 0, 31, 12);
+console.log(localDate - utcDate === utcDate.getTimezoneOffset() * 60 * 1000);
 
-console.log(date1.toISOString());
+var date = new Date();
+date.setUTCFullYear(2000, 0, 31);
+date.setUTCHours(12, 0, 0, 0);
+console.log(date);
 
-console.log(date1.toGMTString());
+var date = new Date(Date.UTC(2000, 0, 31, 12));
+var timestamp = date.getTime();
+//Alternatively
+var timestamp2 = Date.UTC(2000, 0, 31, 12);
+console.log(timestamp === timestamp2);
 
-console.log(date1.toLocaleDateString());
-
-console.log(date1.toLocaleDateString(["zh", "en-US"]));
-
-var options = {
-  weekday: "long",
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-};
-console.log(date1.toLocaleDateString([], options));
+//And when constructing a date from it elsewhere...
+var otherDate = new Date(timestamp);
+//Represented as a universal date
+console.log(otherDate.toUTCString());
+//Represented as a local date
+console.log(otherDate);
