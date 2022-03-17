a8b2a29d3adb4d7db6682f8d775010e7b25584f2
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 18:35:41 +0100

    using `||` and `&&` short circuiting

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const AnimalSays = {
-  dog() {
-    return "woof";
-  },
-  cat() {
-    return "meow";
-  },
-  lion() {
-    return "roar";
-  },
-  // ... other animals
-  default() {
-    return "moo";
-  },
-};
-
-function makeAnimalSpeak(animal) {
-  // Match the animal by type
-  const speak = AnimalSays[animal] || AnimalSays.default;
-  console.log(animal + " says " + speak());
-}
-
-makeAnimalSpeak("dog"); // => 'dog says woof'
-makeAnimalSpeak("cat"); // => 'cat says meow'
-makeAnimalSpeak("lion"); // => 'lion says roar'
-makeAnimalSpeak("snake"); // => 'snake says moo'
+var x = 10;
+x == 10 && console.log("x is 10");
+x == 10 || console.log("x is not 10");
