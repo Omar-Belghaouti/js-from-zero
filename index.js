2eb95cfbbfaf13471fff2b492d35534665993a32
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 18:27:43 +0100

    `switch` statement

--- index.js
+++ index.js
@@ -1,38 +1,68 @@
-var animal = "kitty";
-var result = animal === "kitty" ? "cute" : "still nice";
-console.log(result);
-
-var animal = "kitty";
-var result = "";
-if (animal === "kitty") {
-  result = "cute";
-} else {
-  result = "still nice";
-}
-console.log(result);
-
-var a = 0;
-var str = "not a";
-var b = "";
-b = a === 0 ? ((a = 1), (str += " test")) : (a = 2);
-console.log(a, b);
-
-var a = 1;
-a === 1 ? console.log("Hey, it is 1!") : 0;
-
-var foo, bar;
-foo ? (bar ? 1 : 2) : 3;
-if (foo) {
-  if (bar) {
-    1;
-  } else {
-    2;
-  }
-} else {
-  3;
+var value = 1;
+switch (value) {
+  case 1:
+    console.log("I will always run");
+    break;
+  case 2:
+    console.log("I will never run");
+    break;
 }
 
-// Cannot break within ternaries
-// var animal = 'kitty';
-// for (var i = 0; i < 5; ++i) {
-// (animal === 'kitty') ? break:console.log(i); }
+switch (value) {
+  case 1:
+    console.log("I will only run if value === 1");
+  // Here, the code "falls through" and will run the code under case 2
+  case 2:
+    console.log("I will run if value === 1 or value === 2");
+    break;
+  case 3:
+    console.log("I will only run if value === 3");
+    break;
+}
+
+var animal = "Lion";
+switch (animal) {
+  case "Dog":
+    console.log('I will not run since animal !== "Dog"');
+    break;
+  case "Cat":
+    console.log('I will not run since animal !== "Cat"');
+    break;
+  default:
+    console.log("I will run since animal does not match any other case");
+}
+
+function john() {
+  return "John";
+}
+function jacob() {
+  return "Jacob";
+}
+var name = "John";
+switch (name) {
+  case john(): // Compare name with the return value of john() (name == "John")
+    console.log('I will run if name === "John"');
+    break;
+  case "Ja" + "ne": // Concatenate the strings together then compare (name == "Jane")
+    console.log('I will run if name === "Jane"');
+    break;
+  case john() + " " + jacob() + " Jingleheimer Schmidt":
+    console.log("His name is equal to name too!");
+    break;
+}
+
+// Multiple inclusive criteria for cases
+var x = "c";
+switch (x) {
+  case "a":
+  case "b":
+  case "c":
+    console.log("Either a, b, or c was selected.");
+    break;
+  case "d":
+    console.log("Only d was selected.");
+    break;
+  default:
+    console.log("No case was matched.");
+    break; // precautionary break if case order changes
+}
