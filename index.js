51e20118c491e86bd562c07d18bbb0357cd29830
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 23:14:19 +0100

    array spread / rest

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-var columns = ["Date", "Number", "Size", "Location", "Age"];
-var rows = ["2001", "5", "Big", "Sydney", "25"];
-var result = rows.reduce(function (result, field, index) {
-  result[columns[index]] = field;
-  return result;
-}, {});
-console.log(result);
+var arr = [1, 2, 3, ...[4, 5, 6]]; // [1, 2, 3, 4, 5, 6]
+// in ES < 6, the operations above are equivalent to
+arr = [1, 2, 3];
+arr.push(4, 5, 6);
+
+var arr = [1, 2, 3, ...[..."456"].map((x) => parseInt(x))]; // [1, 2, 3, 4, 5, 6]
+
+var arr = [..."123456"].map((x) => parseInt(x)); // [1, 2, 3, 4, 5, 6]
+
+var arr = [..."123456"]; // ["1", "2", "3", "4", "5", "6"]
+
+function myFunction(a, b, c) {}
+let args = [0, 1, 2];
+myFunction(...args);
+// in ES < 6, this would be equivalent to:
+myFunction.apply(null, args);
+
+[a, b, ...rest] = [1, 2, 3, 4, 5, 6]; // rest is assigned [3, 4, 5, 6]
+
+function myFunction(a, b, ...rest) {
+  console.log(rest);
+}
+myFunction(0, 1, 2, 3, 4, 5, 6); // rest is [2, 3, 4, 5, 6]
