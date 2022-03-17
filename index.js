6b08b1f50f78b7e460093f4969fbda6709bb1d97
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 17:45:55 +0100

    logic operators with booleans

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-1 == 1; // true
-1 == true; // true  (operand converted to number: true => 1)
-1 == "1"; // true  (operand converted to number: '1' => 1 )
-1 == "1.00"; // true
-1 == "1.00000000001"; // false
-1 == "1.00000000000000001"; // true  (true due to precision loss)
-null == undefined; // true
-1 == 2; // false
-0 == false; // true
-0 == undefined; // false
-0 == ""; // true
+var x = true,
+  y = false;
+
+x && y; // false (evaluate x and y)
+x || y; // true (evaluate x)
+!x; // false
