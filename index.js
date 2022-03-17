3d1fc487000b8fba8b2629bf7eaa87db199d5df6
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 17:52:00 +0100

    boolean coercion

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-var x = 5 + 7; // x.valueOf() is 12,  typeof x is a number
-varx = 5 + "7"; // x.valueOf() is 57,  typeof x is a string
-varx = "5" + 7; // x.valueOf() is 57,  typeof x is a string
-var x = 5 - 7; // x.valueOf() is -2,  typeof x is a number
-varx = 5 - "7"; // x.valueOf() is -2,  typeof x is a number
-varx = "5" - 7; // x.valueOf() is -2,  typeof x is a number
-varx = 5 - "x"; // x.valueOf() is NaN, typeof x is a number
+var a = "hello" || ""; // a = 'hello'
+var b = "" || []; // b = []
+var c = "" || undefined; // c = undefined
+var d = 1 || 5; // d = 1
+var e = 0 || {}; // e = {}
+var f = 0 || "" || 5; // f = 5
+var g = "" || "yay" || "boo"; // g = 'yay'
+
+var a = "hello" && ""; // a = ''
+var b = "" && []; // b = ''
+var c = undefined && 0; // c = undefined
+var d = 1 && 5; // d = 5
+var e = 0 && {}; // e = 0
+var f = "hi" && [] && "done"; // f = 'done'
+var g = "bye" && undefined && "adios"; // g = undefined
+
+var foo = function (val) {
+  // if val evaluates to falsey, 'default' will be returned instead.
+  return val || "default";
+};
+console.log(foo("burger")); // burger
+console.log(foo(100)); // 100
+console.log(foo([])); // []
+console.log(foo(0)); // default
+console.log(foo(undefined)); // default
