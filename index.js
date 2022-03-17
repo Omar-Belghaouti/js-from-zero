1816224d1fe300be1eb15f63e4822befb30f95a7
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 23:04:46 +0100

    shallow cloning an array

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-var arr1 = [11, 22, 33];
-var arr2 = arr1.reverse();
-console.log(arr2); // [33, 22, 11]
-console.log(arr1); // [33, 22, 11]
-
-function deepReverse(arr) {
-  arr.reverse().forEach((elem) => {
-    if (Array.isArray(elem)) {
-      deepReverse(elem);
-    }
-  });
-  return arr;
-}
-var arr = [1, 2, 3, [1, 2, 3, ["a", "b", "c"]]];
-deepReverse(arr);
-console.log(arr);
+arrayToClone = [1, 2, 3, 4, 5];
+clone1 = Array.from(arrayToClone);
+clone2 = Array.of(...arrayToClone);
+clone3 = [...arrayToClone]; // the shortest way
