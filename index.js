ddf1beff9f5ef3e2a04fa01f0e17f40ed62df75b
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 18:23:53 +0100

    ternary operators

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
-var age = 24;
-var height = 6.5;
-var status = "normal";
-var hasInvitation = true;
-if ((age >= 18 && height >= 5.11) || (status === "royalty" && hasInvitation)) {
-  console.log("You can enter our club");
+var animal = "kitty";
+var result = animal === "kitty" ? "cute" : "still nice";
+console.log(result);
+
+var animal = "kitty";
+var result = "";
+if (animal === "kitty") {
+  result = "cute";
+} else {
+  result = "still nice";
 }
+console.log(result);
+
+var a = 0;
+var str = "not a";
+var b = "";
+b = a === 0 ? ((a = 1), (str += " test")) : (a = 2);
+console.log(a, b);
+
+var a = 1;
+a === 1 ? console.log("Hey, it is 1!") : 0;
+
+var foo, bar;
+foo ? (bar ? 1 : 2) : 3;
+if (foo) {
+  if (bar) {
+    1;
+  } else {
+    2;
+  }
+} else {
+  3;
+}
+
+// Cannot break within ternaries
+// var animal = 'kitty';
+// for (var i = 0; i < 5; ++i) {
+// (animal === 'kitty') ? break:console.log(i); }
