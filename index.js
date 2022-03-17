86ee676315daeba3ef80a6b53149a33bd9da7b6a
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 17:36:19 +0100

    short-circuiting in boolean operators

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-console.log(1 * "two" === NaN);
-console.log(NaN === 0);
-console.log(NaN === NaN);
-console.log(Number.NaN === NaN);
-console.log(NaN < 0);
-console.log(NaN > 0);
-console.log(NaN >= NaN);
-console.log(NaN >= "two");
+function T() {
+  // True
+  console.log("T");
+  return true;
+}
+function F() {
+  // False
+  console.log("F");
+  return false;
+}
 
-console.log(NaN !== 0);
-console.log(NaN !== NaN);
+console.log(T() || F());
+console.log(F() && T());
+console.log(F() || T());
 
-console.log(Object.is(NaN, NaN));
-console.log(Object.is(+0, -0));
+var obj;
+if (obj !== undefined && obj.property) {
+}
+if (typeof obj === "object" && obj.property) {
+}
 
-console.log(+0 === -0);
+var nullableObj = null;
+var obj = nullableObj || {}; // this selects {}
+var nullableObj2 = { x: 5 };
+var obj2 = nullableObj2 || {}; // this selects {x: 5}
 
-console.log(typeof NaN === "number");
+function myMethod(cb) {
+  // This can be simplified
+  if (cb) {
+    cb();
+  }
+  // To this
+  cb && cb();
+}
