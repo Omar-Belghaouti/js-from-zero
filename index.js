f3579460ef1d9dbe04f94d0a91ed2f9bbb18f081
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 20:12:51 +0100

    converting `Array-like` objects to arrays

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-var x = 10;
-x == 10 && console.log("x is 10");
-x == 10 || console.log("x is not 10");
+var realArray = ["a", "b", "c"];
+var arrayLike = {
+  0: "a",
+  1: "b",
+  2: "c",
+  length: 3,
+};
+
+// arrayLike.forEach((value) => {
+//   /* Do something */
+// }); // Errors
+var realArray = Array.from(arrayLike);
+realArray.forEach((value) => {
+  /* Do something */
+  console.log(value);
+}); // Works
+
+var realArray = Object.values(arrayLike);
+
+var realArray = Object.keys(arrayLike).map((key) => arrayLike[key]);
+
+// arrayLike.forEach(function () {
+//   // Do stuff
+// }); // Error! forEach is not defined.
+[].forEach.bind(arrayLike)(function (val) {
+  // Do stuff with val
+}); // Wow! this works
