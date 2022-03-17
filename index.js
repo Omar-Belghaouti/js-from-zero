16eb5534f6c808ac71d665a974177e0b7a3109f9
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 13:59:34 +0100

    find the index of a substring inside a string

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-var string = "Hello, World!";
+console.log("Hellow World".indexOf("Wor"));
 
-console.log(string.indexOf("o"));
-console.log(string.indexOf("foo"));
+console.log("harr dee harr dee harr".indexOf("dee", 10));
 
-console.log(string.lastIndexOf("o"));
-console.log(string.lastIndexOf("foo"));
-
-console.log(string.includes("Hello"));
-console.log(string.includes("foo"));
-
-string = string.replace("Hello", "Bye");
-console.log(string);
-
-string = string.replace(/W.{3}d/g, "Universe");
-console.log(string);
-
-var str = "heLlo, woRlD!";
-str = str.replace(/([a-zA-Z])([a-zA-Z]+)/g, function (match, g1, g2) {
-  return g1.toUpperCase() + g2.toLowerCase();
-});
-console.log(str);
+console.log("Hellow World".indexOf("WOR"));
