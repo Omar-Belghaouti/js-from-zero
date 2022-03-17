8ecd25b0a8e8d13ff7200de6ad3bdf7bb8257597
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 17:39:50 +0100

    `null` and `undefined`

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-function T() {
-  // True
-  console.log("T");
-  return true;
-}
-function F() {
-  // False
-  console.log("F");
-  return false;
-}
+console.log(null == undefined);
+console.log(null === undefined);
 
-console.log(T() || F());
-console.log(F() && T());
-console.log(F() || T());
+if (null) console.log("won't be logged");
+if (undefined) console.log("won't be logged");
 
-var obj;
-if (obj !== undefined && obj.property) {
-}
-if (typeof obj === "object" && obj.property) {
-}
-
-var nullableObj = null;
-var obj = nullableObj || {}; // this selects {}
-var nullableObj2 = { x: 5 };
-var obj2 = nullableObj2 || {}; // this selects {x: 5}
-
-function myMethod(cb) {
-  // This can be simplified
-  if (cb) {
-    cb();
-  }
-  // To this
-  cb && cb();
-}
+false == undefined; // false
+false == null; // false
+false === undefined; // false
+false === null; // false
