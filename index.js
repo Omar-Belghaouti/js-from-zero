1b1084f66d11e10dd8f7aef684ab88bab3b7d686
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 17:43:24 +0100

    abstract equality

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-console.log(null == undefined);
-console.log(null === undefined);
-
-if (null) console.log("won't be logged");
-if (undefined) console.log("won't be logged");
-
-false == undefined; // false
-false == null; // false
-false === undefined; // false
-false === null; // false
+1 == 1; // true
+1 == true; // true  (operand converted to number: true => 1)
+1 == "1"; // true  (operand converted to number: '1' => 1 )
+1 == "1.00"; // true
+1 == "1.00000000001"; // false
+1 == "1.00000000000000001"; // true  (true due to precision loss)
+null == undefined; // true
+1 == 2; // false
+0 == false; // true
+0 == undefined; // false
+0 == ""; // true
