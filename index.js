f2c31e93a902645d8eb613d68ed66a36849c78bc
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 17:53:58 +0100

    empty array

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-var a = "hello" || ""; // a = 'hello'
-var b = "" || []; // b = []
-var c = "" || undefined; // c = undefined
-var d = 1 || 5; // d = 1
-var e = 0 || {}; // e = {}
-var f = 0 || "" || 5; // f = 5
-var g = "" || "yay" || "boo"; // g = 'yay'
+/* ToNumber(ToPrimitive([])) == ToNumber(false) */
+[] == false; // true
 
-var a = "hello" && ""; // a = ''
-var b = "" && []; // b = ''
-var c = undefined && 0; // c = undefined
-var d = 1 && 5; // d = 5
-var e = 0 && {}; // e = 0
-var f = "hi" && [] && "done"; // f = 'done'
-var g = "bye" && undefined && "adios"; // g = undefined
+// Internally this is evaluated as ToBoolean([]) === true ? 'truthy' : 'falsy'
+[] ? "truthy" : "falsy"; // 'truthy'
 
-var foo = function (val) {
-  // if val evaluates to falsey, 'default' will be returned instead.
-  return val || "default";
-};
-console.log(foo("burger")); // burger
-console.log(foo(100)); // 100
-console.log(foo([])); // []
-console.log(foo(0)); // default
-console.log(foo(undefined)); // default
+// All objects are considered truthy and an array is an instance of object
