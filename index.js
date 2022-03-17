cd4b180e8ca5a712d4db376674f1b5dcbf67fd28
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 14:36:21 +0100

    get the current time and date

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-console.log(Date.now());
+var year = new Date().getFullYear();
+console.log(year);
 
-console.log(new Date().getTime());
+var month = new Date().getMonth();
+console.log(month);
+
+var day = new Date().getDate();
+console.log(day);
+
+var hours = new Date().getHours();
+console.log(hours);
+
+var minutes = new Date().getMinutes();
+console.log(minutes);
+
+var seconds = new Date().getSeconds();
+console.log(seconds);
+
+var milliseconds = new Date().getMilliseconds();
+console.log(milliseconds);
+
+var now = new Date();
+// convert date to a string in UTC timezone format:
+console.log(now.toUTCString());
