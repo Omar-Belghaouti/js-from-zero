bed5d81eab2a20788c0ff10e8212ee8aa5955aba
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 23:10:34 +0100

    merge two arrays as key value pair

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-var array1 = [1, 2];
-var array2 = [3, 4, 5];
-
-var array3 = array1.concat(array2); // returns a new array
-var array3 = [...array1, ...array2];
-
-var array1 = ["a", "b"],
-  array2 = ["c", "d"],
-  array3 = ["e", "f"],
-  array4 = ["g", "h"];
-
-var arrConc = array1.concat(array2, array3, array4);
-var arrConc = [...array1, ...array2, ...array3, ...array4];
-
-// Without copying the first array
-var longArray = [1, 2, 3, 4, 5, 6, 7, 8],
-  shortArray = [9, 10];
-
-longArray.push.apply(longArray, shortArray);
-longArray.push(...shortArray);
-
-shortArray.forEach(function (elem) {
-  longArray.push(elem);
-});
+var columns = ["Date", "Number", "Size", "Location", "Age"];
+var rows = ["2001", "5", "Big", "Sydney", "25"];
+var result = rows.reduce(function (result, field, index) {
+  result[columns[index]] = field;
+  return result;
+}, {});
+console.log(result);
