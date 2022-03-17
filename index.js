b9d5d66e0c96d299d1eec0b6c9d43e5f597f1412
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 14:03:30 +0100

    repeat a string

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-console.log("qwerty".toUpperCase());
+console.log("abc".repeat(3));
 
-console.log("QWERTY".toLowerCase());
+console.log("abc".repeat(0));
+
+// console.log("abc".repeat(-1));
+
+var myString = "abc";
+var n = 3;
+console.log(new Array(n + 1).join("abc"));
