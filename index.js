d2b3654b7b1a3ea32f31282de1d840160db96544
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 20:26:05 +0100

    reducing values

--- index.js
+++ index.js
@@ -1,27 +1,74 @@
-var realArray = ["a", "b", "c"];
-var arrayLike = {
-  0: "a",
-  1: "b",
-  2: "c",
-  length: 3,
-};
+var result = [1, 2, 3, 4].reduce(function (a, b) {
+  console.log(a, b);
+  return a + b;
+});
+console.log("result:", result);
 
-// arrayLike.forEach((value) => {
-//   /* Do something */
-// }); // Errors
-var realArray = Array.from(arrayLike);
-realArray.forEach((value) => {
-  /* Do something */
-  console.log(value);
-}); // Works
+result = [2].reduce(function (a, b) {
+  console.log(a, b); // prints: 1 2
+  return a + b;
+}, 1);
+console.log("result:", result);
 
-var realArray = Object.values(arrayLike);
+// Flattening an array
+var array = [
+  { key: "one", value: 1 },
+  {
+    key: "two",
+    value: 2,
+  },
+  {
+    key: "three",
+    value: 3,
+  },
+];
+var obj = array.reduce(function (obj, current) {
+  obj[current.key] = current.value;
+  return obj;
+}, {});
+console.log("obj:", obj);
 
-var realArray = Object.keys(arrayLike).map((key) => arrayLike[key]);
+obj = array.reduce(
+  (obj, current) =>
+    Object.assign(obj, {
+      [current.key]: current.value,
+    }),
+  {}
+);
+console.log("obj:", obj);
 
-// arrayLike.forEach(function () {
-//   // Do stuff
-// }); // Error! forEach is not defined.
-[].forEach.bind(arrayLike)(function (val) {
-  // Do stuff with val
-}); // Wow! this works
+obj = array.reduce(
+  (obj, current) => ({ ...obj, [current.key]: current.value }),
+  {}
+);
+console.log("obj:", obj);
+
+// Map using reduce
+function map(list, fn) {
+  return list.reduce(function (newList, item) {
+    return newList.concat(fn(item));
+  }, []);
+}
+// Usage:
+result = map([1, 2, 3], function (n) {
+  return n * n;
+}); // → [1, 4, 9]
+console.log("result:", result);
+
+// Find min or max value
+var arr = [4, 2, 1, -10, 9];
+result = arr.reduce(function (a, b) {
+  return a < b ? a : b;
+}, Infinity); // → -10
+console.log("result:", result);
+
+// Find unique values
+var arr = [1, 2, 1, 5, 9, 5];
+result = arr.reduce((prev, number) => {
+  if (prev.indexOf(number) === -1) {
+    prev.push(number);
+  }
+  return prev;
+}, []);
+// → [1, 2, 5, 9]
+console.log("result:", result);
