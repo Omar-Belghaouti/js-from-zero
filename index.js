f6a613b397f53b3f4439a413295075559c0b1a0f
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 17:47:29 +0100

    automatic type conversions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var x = true,
-  y = false;
-
-x && y; // false (evaluate x and y)
-x || y; // true (evaluate x)
-!x; // false
+var x = 5 + 7; // x.valueOf() is 12,  typeof x is a number
+varx = 5 + "7"; // x.valueOf() is 57,  typeof x is a string
+varx = "5" + 7; // x.valueOf() is 57,  typeof x is a string
+var x = 5 - 7; // x.valueOf() is -2,  typeof x is a number
+varx = 5 - "7"; // x.valueOf() is -2,  typeof x is a number
+varx = "5" - 7; // x.valueOf() is -2,  typeof x is a number
+varx = 5 - "x"; // x.valueOf() is NaN, typeof x is a number
