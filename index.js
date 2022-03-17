4a61cc18a22a4a2335e311fab5e65d4205c2de66
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 23:17:18 +0100

    filtering values

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-var arr = [1, 2, 3, ...[4, 5, 6]]; // [1, 2, 3, 4, 5, 6]
-// in ES < 6, the operations above are equivalent to
-arr = [1, 2, 3];
-arr.push(4, 5, 6);
+var filtered = [0, undefined, {}, null, "", true, 5].filter(Boolean);
+console.log(filtered);
 
-var arr = [1, 2, 3, ...[..."456"].map((x) => parseInt(x))]; // [1, 2, 3, 4, 5, 6]
-
-var arr = [..."123456"].map((x) => parseInt(x)); // [1, 2, 3, 4, 5, 6]
-
-var arr = [..."123456"]; // ["1", "2", "3", "4", "5", "6"]
-
-function myFunction(a, b, c) {}
-let args = [0, 1, 2];
-myFunction(...args);
-// in ES < 6, this would be equivalent to:
-myFunction.apply(null, args);
-
-[a, b, ...rest] = [1, 2, 3, 4, 5, 6]; // rest is assigned [3, 4, 5, 6]
-
-function myFunction(a, b, ...rest) {
-  console.log(rest);
+function startsWithLetterA(str) {
+  if (str && str[0].toLowerCase() == "a") {
+    return true;
+  }
+  return false;
 }
-myFunction(0, 1, 2, 3, 4, 5, 6); // rest is [2, 3, 4, 5, 6]
+var str =
+  "Since Boolean is a native javascript function/constructor that takes [one optional parameter] and the filter method also takes a function and passes it the current array item as a parameter, you could read it like the following";
+var strArray = str.split(" ");
+var wordsStartsWithA = strArray.filter(startsWithLetterA); //["a", "and", "also", "a", "and", "array", "as"]
+console.log(wordsStartsWithA);
