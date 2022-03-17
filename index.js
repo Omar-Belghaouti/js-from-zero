957f8c8db38f4a558ecdcc084a55b7a3a820c4f8
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 23:21:51 +0100

    convert a string to an array

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-var people = [{ name: "bob" }, { name: "john" }];
-var bob = people.find((person) => person.name === "bob");
-console.log(bob);
+var strArray = "StackOverflow".split("");
+console.log(strArray);
 
-for (var i = 0; i < people.length; i++) {
-  if (people[i].name === "bob") {
-    break; // we found bob
-  }
-}
-
-array = [{ value: 1 }, { value: 2 }, { value: 3 }, { value: 4 }, { value: 5 }];
-
-var index = array.findIndex((item) => item.value === 3); // 2
-var index = array.findIndex((item) => item.value === 12); // -1
+var strArray = [..."sky is blue"];
+console.log(strArray);
