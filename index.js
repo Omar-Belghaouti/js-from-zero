d70e0ad28a3ff8b78f6f3543603a532919970320
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-17 23:27:46 +0100

    removing items from an array

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-var strArray = "StackOverflow".split("");
-console.log(strArray);
+// Shift
+var array = [1, 2, 3, 4];
+array.shift();
+console.log(array);
 
-var strArray = [..."sky is blue"];
-console.log(strArray);
+// Pop
+var array = [1, 2, 3];
+array.pop();
+console.log(array);
+
+// Splice
+var array = [1, 2, 3, 4];
+array.splice(1, 2);
+console.log(array);
+
+var array = [1, 2, 3, 4];
+var res = array.splice(2);
+console.log(array);
+console.log(res);
+
+// Delete
+var array = [1, 2, 3, 4, 5];
+console.log(array.length); // 5
+delete array[2];
+console.log(array); // [1, 2, undefined, 4, 5]
+console.log(array.length); // 5
+
+array = [1, 2, 3, 4, 5];
+array.length = 2;
+console.log(array); // [1, 2]
