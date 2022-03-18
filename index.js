fde414e17609551290a5ad8fb221311bf328e31e
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 14:25:34 +0100

    shallow cloning an object

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-// Let's say we have this Array of Alphabets
-var arr = ["a", "b", "c", "d"];
-// I want an Array of the first two Alphabets
-var newArr = arr.slice(0, 2); // newArr === ["a", "b"]
+const existing = { a: 1, b: 2, c: 3 };
+const clone = Object.assign({}, existing);
+console.log(clone);
 
-// Let's say we have this Array of Numbers
-// and I don't know it's end
-var arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-// I want to slice this Array starting from
-// number 5 to its end
-var newArr = arr.slice(4); // newArr === [4, 5, 6, 7, 8, 9]
+const { ...clone2 } = existing;
+console.log(clone2);
+
+var clone3 = {};
+for (const prop in existing) {
+  if (existing.hasOwnProperty(prop)) {
+    clone3[prop] = existing[prop];
+  }
+}
+console.log(clone3);
