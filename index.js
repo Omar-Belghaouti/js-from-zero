f2c7f36a68cc7eb9ccd8f9600158accb5096d34d
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 10:13:19 +0100

    removing / adding elements using `splice()`

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log(["Hello", " ", "world"].join("")); // "Hello world"
-console.log([1, 800, 555, 1234].join("-")); // "1-800-555-1234"
+var values = [1, 2, 3, 4, 5, 3];
+var i = values.indexOf(3);
+if (i >= 0) {
+  values.splice(i, 1);
+}
+// [1, 2, 4, 5, 3]
+
+var values = [1, 2, 4, 5, 3];
+var i = values.length + 1;
+values.splice(i, 0, 6, 7, 8); //[1, 2, 4, 5, 3, 6, 7, 8]
