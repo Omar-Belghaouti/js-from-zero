4a6694817dc11da71828a41aea840af1a8135c6d
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 16:47:51 +0100

    `Object.assign()`

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-var object = {};
+// Add a property
+var user = { firstName: "John" };
+Object.assign(user, { lastName: "Doe", age: 39 });
+console.log(user); // Logs: {firstName: "John", lastName: "Doe", age: 39}
 
-for (var property in object) {
-  // always check if an object has a property
-  if (object.hasOwnProperty(property)) {
-    // do stuff
-  }
-}
+// Create a shallow copy
+var obj = Object.assign({}, user);
+console.log(obj); // Logs: {firstName: "John", lastName: "Doe", age: 39}
 
-var obj = { 0: "a", 1: "b", 2: "c" };
-Object.keys(obj).map(function (key) {
-  console.log(key, obj[key]);
-});
+// Merge many properties from multiple objects
+var obj1 = { a: 1 };
+var obj2 = { b: 2 };
+var obj3 = { c: 3 };
+var obj = Object.assign(obj1, obj2, obj3);
+console.log(obj); // Logs: { a: 1, b: 2, c: 3 }
+console.log(obj1); // Logs: { a: 1, b: 2, c: 3 }, target object itself is changed
+
+// Primitives will be wrapped, null and undefined will be ignored
+var var_1 = "abc";
+var var_2 = true;
+var var_3 = 10;
+var var_4 = Symbol("foo");
+var obj = Object.assign({}, var_1, null, var_2, undefined, var_3, var_4);
+console.log(obj); // Logs: { "0": "a", "1": "b", "2": "c" }
