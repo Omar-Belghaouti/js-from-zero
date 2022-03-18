1230fe947d6f9922d4bae01d1ecaea75b4a23b9b
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 10:08:06 +0100

    joining array elements in a string

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-var arr = [1, 2, 3, 4];
-var arr2 = new Array(1, 2, 3, 4);
-var arr3 = new Array(); // []
-var arr4 = new Array(4); // 4 length array with undefined elements
-var arr5 = new Array("foo"); // ["foo"]
-var arr6 = Array.of(21, "Hello", "World"); // [21, "Hello", "World"]
-var newArray = Array.from({ length: 5 }, (_, index) => Math.pow(index, 4)); // [0, 1, 16, 81, 256]
+console.log(["Hello", " ", "world"].join("")); // "Hello world"
+console.log([1, 800, 555, 1234].join("-")); // "1-800-555-1234"
