2ee5d83acc3e5c7b0b1fd792eec52aeab656d2cd
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 17:13:20 +0100

    convert object's values to array

--- index.js
+++ index.js
@@ -1,35 +1,10 @@
 var obj = {
-  foo: "foo",
-  bar: function () {
-    return "bar";
-  },
+  a: "hello",
+  b: "this is",
+  c: "javascript!",
 };
-Object.seal(obj); // unlike Object.freeze(), this can make properties editable
-obj.newFoo = "newFoo";
-obj.bar = function () {
-  return "foo";
-};
-obj.newFoo; // undefined
-obj.bar(); // 'foo'
 
-// Can't make foo an accessor property
-Object.defineProperty(obj, "foo", {
-  get: function () {
-    return "newFoo";
-  },
-}); // TypeError
-
-// But you can make it read only
-Object.defineProperty(obj, "foo", {
-  writable: false,
-}); // TypeError
-obj.foo = "newFoo";
-obj.foo; // 'foo';
-
-// In strict mode
-(function () {
-  "use strict";
-  var obj = { foo: "foo" };
-  Object.seal(obj);
-  obj.newFoo = "newFoo"; // TypeError
-})();
+var array = Object.keys(obj).map(function (key) {
+  return obj[key];
+});
+console.log(array); // ["hello", "this is", "javascript!"]
