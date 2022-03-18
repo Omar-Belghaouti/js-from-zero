23ce5a2c8bdac5408d1559eaee3cec0612a4cf32
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 10:15:12 +0100

    the `entries()` method

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-var values = [1, 2, 3, 4, 5, 3];
-var i = values.indexOf(3);
-if (i >= 0) {
-  values.splice(i, 1);
+var letters = ["a", "b", "c"];
+for (const [index, element] of letters.entries()) {
+  console.log(index, element);
 }
-// [1, 2, 4, 5, 3]
-
-var values = [1, 2, 4, 5, 3];
-var i = values.length + 1;
-values.splice(i, 0, 6, 7, 8); //[1, 2, 4, 5, 3, 6, 7, 8]
