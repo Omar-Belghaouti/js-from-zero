2571087d1b9f173e80ec5d08437e84ad7d13d6d7
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 10:36:10 +0100

    insert an item into an array at a specific index

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-Array.isArray([]); // true
-Array.isArray([1, 2, 3]); // true
-Array.isArray({}); // false
-Array.isArray(1); // false
-
-[] instanceof Array; // true
-var obj = {};
-obj instanceof Array; // false
-
-var arr = [];
-Object.setPrototypeOf(arr, null);
-Array.isArray(arr); // true
-arr instanceof Array; // false
+var arr = [1, 2, 3];
+var index = 1;
+var item = 4;
+arr.splice(index, 0, item);
+console.log(arr);
