0d53c36cf67dc9c4648e85566ba9b3b39023c38d
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 16:43:47 +0100

    object properties iteration

--- index.js
+++ index.js
@@ -1,45 +1,13 @@
-var existing = { a: 1, b: { c: 2 } };
-var copy = JSON.parse(JSON.stringify(existing));
-existing.b.c = 3; // copy.b.c will not change
+var object = {};
 
-function deepClone(obj) {
-  function clone(obj, traversedObjects) {
-    var copy;
-    // primitive types
-    if (obj === null || typeof obj !== "object") {
-      return obj;
-    }
-    // detect cycles
-    for (var i = 0; i < traversedObjects.length; i++) {
-      if (traversedObjects[i] === obj) {
-        throw new Error("Cannot clone circular object.");
-      }
-    }
-    // dates
-    if (obj instanceof Date) {
-      copy = new Date();
-      copy.setTime(obj.getTime());
-      return copy;
-    }
-    // arrays
-    if (obj instanceof Array) {
-      copy = [];
-      for (var i = 0; i < obj.length; i++) {
-        copy.push(clone(obj[i], traversedObjects.concat(obj)));
-      }
-      return copy;
-    }
-    // simple objects
-    if (obj instanceof Object) {
-      copy = {};
-      for (var key in obj) {
-        if (obj.hasOwnProperty(key)) {
-          copy[key] = clone(obj[key], traversedObjects.concat(obj));
-        }
-      }
-      return copy;
-    }
-    throw new Error("Not a cloneable object.");
+for (var property in object) {
+  // always check if an object has a property
+  if (object.hasOwnProperty(property)) {
+    // do stuff
   }
-  return clone(obj, []);
 }
+
+var obj = { 0: "a", 1: "b", 2: "c" };
+Object.keys(obj).map(function (key) {
+  console.log(key, obj[key]);
+});
