184a1d22b541c0eda9c67847daa43438ab0c5692
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 17:44:38 +0100

    iterating over object entries with `Object.entries()`

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-var myIterableObject = {};
-// An Iterable object must define a method located at the Symbol.iterator key:
-myIterableObject[Symbol.iterator] = function () {
-  // The iterator should return an Iterator object
-  return {
-    // The Iterator object must implement a method, next()
-    next: function () {
-      // next must itself return an IteratorResult object
-      if (!this.iterated) {
-        this.iterated = true;
-        // The IteratorResult object has two properties
-        return {
-          // whether the iteration is complete, and
-          done: false,
-          // the value of the current iteration value: 'One'
-        };
-      }
-      return {
-        // When iteration is complete, just the done property is needed
-        done: true,
-      };
-    },
-    iterated: false,
-  };
-};
+const obj = { one: 1, two: 2, three: 3 };
+var res = Object.entries(obj);
+console.log(res);
 
-for (var c of myIterableObject) {
-  console.log(c);
+for (const [key, value] of Object.entries(obj)) {
+  console.log(key); // "one", "two" and "three"
+  console.log(value); // 1, 2 and 3
 }
