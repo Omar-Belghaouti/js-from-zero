ada66ac9a3ceee565344388466357bcc061e72a9
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 10:23:33 +0100

    flattening arrays

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-var array = [1, 2, 3];
-var to_remove = 2;
-var array2 = array.filter(function (val) {
-  return val !== to_remove;
-});
-console.log(array2);
-
-// This will remove all elements with value of 2
-while ((index = array.indexOf(2) !== -1)) {
-  array.splice(index, 1);
+// 2 Dimensional arrays
+function flattenES6(arr) {
+  return [].concat(...arr);
 }
-console.log(array);
+var arrL1 = [1, 2, [3, 4]];
+console.log(flattenES6(arrL1)); // [1, 2, 3, 4]
 
-// To remove only the first found element
-var index = array.indexOf(to_remove);
-if (index !== -1) {
-  array.splice(index, 1);
+function flatten(arr) {
+  return [].concat.apply([], arr);
 }
+var arrL1 = [1, 2, [3, 4]];
+console.log(flatten(arrL1)); // [1, 2, 3, 4]
+
+// Higher dimension arrays
+var deeplyNested = [4, [5, 6, [7, 8], 9]];
+console.log(String(deeplyNested).split(",").map(Number));
