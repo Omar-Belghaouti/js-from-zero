4a720fc56e590d42ba163616438d55f84ad5044c
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 16:49:34 +0100

    object rest / spread

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-// Add a property
-var user = { firstName: "John" };
-Object.assign(user, { lastName: "Doe", age: 39 });
-console.log(user); // Logs: {firstName: "John", lastName: "Doe", age: 39}
+let obj = { a: 1 };
+let obj2 = { ...obj, b: 2, c: 3 };
+console.log(obj2); // { a: 1, b: 2, c: 3 };
 
-// Create a shallow copy
-var obj = Object.assign({}, user);
-console.log(obj); // Logs: {firstName: "John", lastName: "Doe", age: 39}
-
-// Merge many properties from multiple objects
-var obj1 = { a: 1 };
-var obj2 = { b: 2 };
-var obj3 = { c: 3 };
-var obj = Object.assign(obj1, obj2, obj3);
-console.log(obj); // Logs: { a: 1, b: 2, c: 3 }
-console.log(obj1); // Logs: { a: 1, b: 2, c: 3 }, target object itself is changed
-
-// Primitives will be wrapped, null and undefined will be ignored
-var var_1 = "abc";
-var var_2 = true;
-var var_3 = 10;
-var var_4 = Symbol("foo");
-var obj = Object.assign({}, var_1, null, var_2, undefined, var_3, var_4);
-console.log(obj); // Logs: { "0": "a", "1": "b", "2": "c" }
+let obj3 = { ...obj, b: { c: 2 } };
+console.log(obj3); // { a: 1, b: { c: 2 } };
