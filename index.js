826a5d1fd14ffae2eb34876da2d673bb882d6882
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-18 10:02:02 +0100

    findinf the minimum or maximum element

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-// Method 1
-var arr = [1, 2, 3, 4];
-arr = []; // reference a to a new array (memory leak)
-console.log(arr);
+var myArray = [1, 2, 3, 4];
+Math.min.apply(null, myArray); // 1
+Math.max.apply(null, myArray); // 4
 
-var count = 0;
-function addListener(arr) {
-  // arr is closed over
-  var b = document.body.querySelector("#foo" + count++);
-  b.addEventListener("click", function (e) {
-    // this functions reference keeps
-    // the closure current while the
-    // event is active
-    // do something but does not need arr
-  });
+var myArray = [1, 2, 3, 4, 99, 20];
+var maxValue = Math.max(...myArray); // 99
+var minValue = Math.min(...myArray); // 1
+
+var maxValue = myArray[0];
+for (var i = 1; i < myArray.length; i++) {
+  var currentValue = myArray[i];
+  if (currentValue > maxValue) {
+    maxValue = currentValue;
+  }
 }
-arr = ["big data"];
-var i = 100;
-while (i > 0) {
-  addListener(arr); // the array is passed to the function
-  arr = []; // only removes the reference, the original array remains
-  array.push("some large data"); // more memory allocated
-  i--;
+
+var myArray = [1, 2, 3, 4];
+myArray.reduce(function (a, b) {
+  return Math.min(a, b);
+}); // 1
+myArray.reduce(function (a, b) {
+  return Math.max(a, b);
+}); // 4
+
+myArray.reduce((a, b) => Math.min(a, b)); // 1
+myArray.reduce((a, b) => Math.max(a, b)); // 4
+
+function myMax(array) {
+  return array.reduce(function (maxSoFar, element) {
+    return Math.max(maxSoFar, element);
+  }, -Infinity);
 }
-// there are now 100 arrays closed over, each referencing a different array
-// no a single item has been deleted
-
-// Method 2
-arr.length = 0;
-
-// Method 3
-arr.splice(0); // should not use if you don't want the removed items
-// only use this method if you do the following
-var keepArr = arr.splice(0); // empties the array and creates a new array containing the removed items
+myMax([3, 5]); // 5
+myMax([]); // -Infinity
+Math.max.apply(null, []); // -Infinity
