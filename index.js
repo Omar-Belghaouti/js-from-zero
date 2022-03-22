1dc4ac83faabc19de65b9992815a091f641594df
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-22 10:51:19 +0100

    remainder / modulus `%`

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-console.log(Math.E);
-console.log(Math.LN10);
-console.log(Math.LOG10E);
-console.log(Math.LOG2E);
-console.log(Math.PI);
-console.log(Math.SQRT1_2);
-console.log(Math.SQRT2);
+console.log(42 % 10); // 2
+console.log(42 % -10); // 2
+console.log(-42 % 10); // -2
+console.log(-42 % -10); // -2
+console.log(-40 % 10); // -0
+console.log(40 % 10); // 0
 
-console.log(Number.EPSILON);
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(Number.MAX_VALUE);
-console.log(Number.MIN_SAFE_INTEGER);
-console.log(Number.MIN_VALUE);
-console.log(Number.NEGATIVE_INFINITY);
-console.log(Number.POSITIVE_INFINITY);
-console.log(Infinity);
+var n = 9; // given n
+var i = 0;
+function delta(d) {
+  // d - any signed integer
+  i = (i + d + n) % n; // we add n to (i+d) to ensure the sum is positive
+}
+
+var myNum = 10 / 4; // 2.5
+var fraction = myNum % 1; // 0.5
+myNum = -20 / 7; // -2.857142857142857
+fraction = myNum % 1; // -0.857142857142857
