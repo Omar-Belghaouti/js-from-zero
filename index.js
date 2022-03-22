b0b67e1026280ded60b752e79cc65b5322b19f21
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-22 11:26:01 +0100

    multiplication `*`

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-var a = 9;
-var b = 3;
-var c = a - b;
-
-"5" - 1; // 4
-7 - "3"; // 4
-"5" - true; // 4
-
-"foo" - 1; // NaN
-100 - "bar"; // NaN
+console.log(3 * 5); // 15
+console.log(-3 * 5); // -15
+console.log(3 * -5); // -15
+console.log(-3 * -5); // 15
