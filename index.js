4da0222a4221e59122a73474701413eead8f9ec4
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-22 11:27:29 +0100

    getting maximum and minimum

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-console.log(3 * 5); // 15
-console.log(-3 * 5); // -15
-console.log(3 * -5); // -15
-console.log(-3 * -5); // 15
+Math.max(4, 12); // 12
+Math.max(-1, -15); // -1
+
+Math.min(4, 12); // 4
+Math.min(-1, -15); // -15
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  max = Math.max.apply(Math, arr),
+  min = Math.min.apply(Math, arr);
+console.log(max); // Logs: 9
+console.log(min); // Logs: 1
+
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  max = Math.max(...arr),
+  min = Math.min(...arr);
+console.log(max); // Logs: 9
+console.log(min); // Logs: 1
