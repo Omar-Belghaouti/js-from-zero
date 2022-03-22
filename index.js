d9021eefcdd2e38d1d409800ffb818610987204b
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-22 10:47:24 +0100

    arithmetic constants

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-var obj = { 0: "a", 1: "b", 2: "c" };
-console.log(Object.values(obj)); // ['a', 'b', 'c']
+console.log(Math.E);
+console.log(Math.LN10);
+console.log(Math.LOG10E);
+console.log(Math.LOG2E);
+console.log(Math.PI);
+console.log(Math.SQRT1_2);
+console.log(Math.SQRT2);
+
+console.log(Number.EPSILON);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MIN_VALUE);
+console.log(Number.NEGATIVE_INFINITY);
+console.log(Number.POSITIVE_INFINITY);
+console.log(Infinity);
