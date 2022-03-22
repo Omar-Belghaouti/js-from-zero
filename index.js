ff58e9be7e36349a257e6947ebdbdfa7be4e2180
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-22 11:31:21 +0100

    getting roots of a number

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-function clamp(min, max, val) {
-  return Math.min(Math.max(min, +val), max);
-}
-console.log(clamp(-10, 10, "4.30")); // 4.3
-console.log(clamp(-10, 10, -8)); // -8
-console.log(clamp(-10, 10, 12)); // 10
-console.log(clamp(-10, 10, -15)); // -10
+Math.sqrt(16); // 4
+
+Math.cbrt(27); // 3
+
+Math.pow(64, 1 / 6); // 2
