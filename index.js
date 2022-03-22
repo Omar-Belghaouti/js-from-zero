58423f973d57844704c3f94e6e6984daebbd7e5e
Omar-Belghaouti <omarbelghaouti@gmail.com>
2022-03-22 11:28:40 +0100

    restrict number to min / max range

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-Math.max(4, 12); // 12
-Math.max(-1, -15); // -1
-
-Math.min(4, 12); // 4
-Math.min(-1, -15); // -15
-
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  max = Math.max.apply(Math, arr),
-  min = Math.min.apply(Math, arr);
-console.log(max); // Logs: 9
-console.log(min); // Logs: 1
-
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  max = Math.max(...arr),
-  min = Math.min(...arr);
-console.log(max); // Logs: 9
-console.log(min); // Logs: 1
+function clamp(min, max, val) {
+  return Math.min(Math.max(min, +val), max);
+}
+console.log(clamp(-10, 10, "4.30")); // 4.3
+console.log(clamp(-10, 10, -8)); // -8
+console.log(clamp(-10, 10, 12)); // 10
+console.log(clamp(-10, 10, -15)); // -10
